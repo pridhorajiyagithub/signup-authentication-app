a6aac32a871085f81ac99c219f13680f48f0e180
pridhorajiyagithub <111557931+pridhorajiyagithub@users.noreply.github.com>
2022-11-18 12:20:00 +0530

    Update home page.dart

--- lib/screens/home page.dart
+++ lib/screens/home page.dart
@@ -1,7 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import '../global.dart';
 import '../helper/firebase_auth_helper.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,13 +13,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    User? data = ModalRoute.of(context)!.settings.arguments as User?;
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "Home Page",
           style: GoogleFonts.ubuntu(),
         ),
-        backgroundColor: Color(0xffa58fd2),
+        backgroundColor: const Color(0xffa58fd2),
         actions: [
           IconButton(
               onPressed: () async {
@@ -28,7 +28,7 @@ class _HomePageState extends State<HomePage> {
                 Navigator.of(context)
                     .pushNamedAndRemoveUntil("/", (route) => false);
               },
-              icon: Icon(Icons.logout))
+              icon: const Icon(Icons.logout))
         ],
       ),
       body: Container(
@@ -37,6 +37,74 @@ class _HomePageState extends State<HomePage> {
           "Welcome Home",
           style: GoogleFonts.ubuntu(
             fontSize: 20,
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: const Color(0xffa58fd2),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              Center(
+                child: CircleAvatar(
+                  radius: 80,
+                  backgroundImage: NetworkImage("${data!.photoURL}"),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Divider(
+                color: Colors.black,
+                thickness: 1.5,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              (data!.displayName != null)
+                  ? Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        "Name = ${data!.displayName}",
+                        textAlign: TextAlign.left,
+                        style:
+                            const TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    )
+                  : const Padding(
+                      padding: EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        "Name = ---",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+              const SizedBox(
+                height: 10,
+              ),
+              (data.email != null)
+                  ? Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        "Gmail = ${data.email}",
+                        textAlign: TextAlign.left,
+                        style:
+                            const TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    )
+                  : const Padding(
+                      padding: EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        "Gmail = ---",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+            ],
           ),
         ),
       ),
